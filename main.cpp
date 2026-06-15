b0f8001e1219c98934b76bd65beae7ffb7240ec1
Ryuu <Aleksander.hagland@hotmail.com>
2026-06-15 23:12:57 +0200

    flush consoll med std::endl for å sikre at all output blir skrevet ut umiddelbart.

--- main.cpp
+++ main.cpp
@@ -5,5 +5,5 @@
 int main() {
  // 1) basic console i/o
  std::println("Hello {}!", "print") ;  //la til ln for linje skifte
- std::cout << "Hello, cout!"  << std::endl; // la til std::endl for linje skifte
+ std::cout << "Hello, cout!"  << std::endl; // la til std::endl for linje skifte og flusher console output buffer
 }
